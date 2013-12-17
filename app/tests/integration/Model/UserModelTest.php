dfb49eefd56a59ff1b500d177d29a3e1e81d9fbb
John Kevin Basco <basco.johnkevin@gmail.com>
2013-12-17 16:25:44 +0800

    add tests for User model

---
+++ app/tests/integration/Model/UserModelTest.php
@@ -0,0 +1,37 @@
+<?php namespace Tests\Integration\Model;
+
+use TestCase;
+use User;
+use Hash;
+
+class UserModelTest extends TestCase {
+
+    public function testGetFullNameAttribute()
+    {
+        $user = new User;
+        $user->first_name = 'John';
+        $user->last_name = 'Doe';
+
+        $this->assertEquals('John Doe', $user->full_name);
+    }
+
+    public function testSetPasswordAttribute()
+    {
+        $user = new User;
+        $user->password = 'myawesomepassword';
+
+        $this->assertTrue((Hash::check('myawesomepassword', $user->password)));
+    }
+
+    public function testGetAbsPhotoUrlAttribute()
+    {
+        $user = new User;
+
+        $this->assertEquals(asset('/assets/images/default-user-avatar.jpeg'), $user->abs_photo_url);
+
+        $user->photo_url = 'photo.png';
+
+        $this->assertEquals(asset('photo.png'), $user->abs_photo_url);
+    }
+
+}
---
+++ app/tests/unit/Model/UserModelTest.php
@@ -0,0 +1,19 @@
+<?php namespace Tests\Unit\Model;
+
+use TestCase;
+use User;
+
+class UserModelTest extends TestCase {
+
+    public function testIsActive()
+    {
+        $user = new User;
+        $user->active = 1;
+
+        $this->assertTrue($user->isActive());
+
+        $user->active = 0;
+        $this->assertFalse($user->isActive());
+    }
+
+}
