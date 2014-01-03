18f254356a4574baf6807f383898dc0e7015a06d
Christopher Pitt <cgpitt@gmail.com>
2014-01-03 21:50:21 +0200

    Fix to bootstrap/start.php

--- bootstrap/start.php
+++ bootstrap/start.php
@@ -24,9 +24,11 @@ $app = new Illuminate\Foundation\Application;
 |
 */
 
-$env = $app->detectEnvironment(function() {
-  return "local";
-});
+$env = $app->detectEnvironment(array(
+  'local' => array('laravelsnippets.dev', 'JOHNs-MacBook-Pro.local'),
+  'testing' => array('laravelsnippets.testing'),
+  'production' => array('mayonvolcanosoftware.com'),
+));
 
 /*
 |--------------------------------------------------------------------------
