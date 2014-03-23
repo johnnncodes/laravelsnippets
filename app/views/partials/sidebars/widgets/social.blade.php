0b4dc27c4ef55961407d377a0f7ebdd5429f62d2
David Knight <david.knight@euged.com>
2014-03-23 14:59:28 +0000

    Fixing bug where profile icons were the wrong way around. Oops!

--- app/views/partials/sidebars/widgets/social.blade.php
+++ app/views/partials/sidebars/widgets/social.blade.php
@@ -4,7 +4,7 @@
 	<p>Getting involved is easy, you can either contribute towards the site's code by forking the <a href="{{ Config::get('site.repo_url') }}">github repo</a> or <a href="{{ route('member.snippet.getCreate') }}">submit your own useful code snippets</a>.</p>
 	<p>Follow us for new snippets, other useful resources that we find from time to time and of course to chat - see you there!</p>
 	<ul class="widget-social-icons">
-		<li><a href="{{ Config::get('site.twitter_url') }}" target="_blank" class="facebook" title="{{ '@' . Config::get('site.twitter_username') }} on Twitter"><i class="fa fa-facebook"></i></a></li>
-		<li><a href="{{ Config::get('site.facebook_url') }}" target="_blank" class="twitter" title="{{ Config::get('site.facebook_username') }} on Facebook"><i class="fa fa-twitter"></i></a></li>
+		<li><a href="{{ Config::get('site.twitter_url') }}" target="_blank" class="twitter" title="{{ '@' . Config::get('site.twitter_username') }} on Twitter"><i class="fa fa-twitter"></i></a></li>
+		<li><a href="{{ Config::get('site.facebook_url') }}" target="_blank" class="facebook" title="{{ Config::get('site.facebook_username') }} on Facebook"><i class="fa fa-facebook"></i></a></li>
 	</ul>
 </div>
