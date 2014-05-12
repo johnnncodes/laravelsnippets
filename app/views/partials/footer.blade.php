7e16590588f883e896b1ba5acafa2fdaf1b8af03
David Knight <david.knight@euged.com>
2014-05-12 20:20:02 +0100

    Updating footer design and adding DigitalOcean link and logo.

--- app/views/partials/footer.blade.php
+++ app/views/partials/footer.blade.php
@@ -6,43 +6,39 @@
 
 <div class="footer">
 	<div class="container">
-		<div class="row">
 
-			<div class="col-md-6">
-				<ul class="footer-links list-inline">
+		<ul class="footer-links list-inline">
 
-					<li><a href="{{ route('home') }}">SNIPPETS</a></li>
+			<li><a href="{{ route('home') }}">SNIPPETS</a></li>
 
-					@if(Auth::check())
-						<li><a href="{{ route('member.user.dashboard') }}">DASHBOARD</a></li>
-					@endif
+			@if(Auth::check())
+				<li><a href="{{ route('member.user.dashboard') }}">DASHBOARD</a></li>
+			@endif
 
-					<li><a href="{{ route('user.getIndex') }}">MEMBERS</a></li>
+			<li><a href="{{ route('user.getIndex') }}">MEMBERS</a></li>
 
-					@if(Auth::check() && Auth::user()->isAdmin())
-						<!-- <li><a href="#">All Snippets</a></li> -->
-					@endif
+			@if(Auth::check() && Auth::user()->isAdmin())
+				<!-- <li><a href="#">All Snippets</a></li> -->
+			@endif
 
-					@if(Auth::check())
-						<li>
-							<!--<li><a href="#">Profile</a></li>-->
-							<a href="{{ route('auth.getLogout') }}">SIGN OUT</a>
-						</li>
-					@else
-						<li>
-							<a href="{{ route('auth.getLogin') }}">SIGN IN</a>
-						</li>
-					@endif
+			@if(Auth::check())
+				<li>
+					<!--<li><a href="#">Profile</a></li>-->
+					<a href="{{ route('auth.getLogout') }}">SIGN OUT</a>
+				</li>
+			@else
+				<li>
+					<a href="{{ route('auth.getLogin') }}">SIGN IN</a>
+				</li>
+			@endif
 
-					<li><a href="{{ route('member.snippet.getCreate') }}">SUBMIT</a></li>
+			<li><a href="{{ route('member.snippet.getCreate') }}">SUBMIT</a></li>
 
-				</ul>
-			</div>
+		</ul>
 
-			<div class="col-md-6">
-				<p class="footer-copyright">{{ Config::get('site.footer_copyright') }}</p>
-			</div>
+		<p class="digitalocean"><a href="http://www.digitalocean.com/" target="_blank" title="Hosting kindly provided by DigitalOcean"><img src="{{ asset('assets/images/digitalocean.png') }}"></a></p>
 
-		</div>
+		<p class="footer-copyright">{{ Config::get('site.footer_copyright') }}</p>
+
 	</div>
 </div>
